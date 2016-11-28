f64177fc9e87dd363834aa161e98e5d69cf3dd40
JakubParzonka <jakub.parzonka@outlook.com>
2016-11-28 21:58:15 +0100

    First FDTI library use

--- app/src/main/java/com/jparzonka/time_interval_app/MainActivity.java
+++ app/src/main/java/com/jparzonka/time_interval_app/MainActivity.java
@@ -2,19 +2,24 @@ package com.jparzonka.time_interval_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.jparzonka.mylibrary.j2xx.D2xxManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textView = (TextView)findViewById(R.id.textView);
         System.out.println(Boolean.valueOf(String.valueOf(0)));
         try {
             D2xxManager manager = D2xxManager.getInstance(getApplicationContext());
+          // System.out.println("Library version: " + D2xxManager.getLibraryVersion());
+            textView.setText(String.valueOf(D2xxManager.getLibraryVersion()));
         } catch (D2xxManager.D2xxException e) {
             e.printStackTrace();
         }
